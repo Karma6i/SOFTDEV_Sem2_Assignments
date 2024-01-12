7ea18a8076726d0a8832143c9f88ca867f1bf016
Marisa Velazquez <velazmar@warren.k12.in.us>
2024-01-12 11:01:23 -0500

    1.5.2

--- 05_Try with Exceptions/Assignment/src/App.java
+++ 05_Try with Exceptions/Assignment/src/App.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+        try(Scanner input = new Scanner(System.in)){ 
+
+        System.out.println("Enter a number: ");
+        String number1 = input.next();
+
+        System.out.println("Enter aother number: ");
+        String number2 = input.next();
+
+        int number1Int = 0;
+        int number2Int = 0;
+
+        }
+
+        try{
+            try{
+                int number1Int = Integer.parseInt(number1);
+            }catch(Exception ex){
+                number1Int = 0;
+                throw new IntegerException("Number 1 is not an integer");
+            }
+            try{
+                number2Int = Integer.parseInt(number2);
+            }catch(Exception ex){
+                number2Int = 0;
+                throw new IntegerException("Number 2 is not an integer");
+            }
+
+        }catch(IntegerException intEX){
+        number1Int = 0;
+        System.out.println(intEX.getMessage());
+        }
+        System.out.println(number1Int * number2Int);
+}
 }
