2b417dbbb4ff19f8e6ec38b7fe934ce4e36185ad
Marisa Velazquez <velazmar@warren.k12.in.us>
2024-03-01 10:59:39 -0500

    6.2.1 Random double

--- Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
+++ Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
@@ -1,5 +1,17 @@
+import java.util.concurrent.CompletableFuture;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double num = Math.random();
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(
+            () -> ("Your number is " + num)
+        );
+
+        CompletableFuture<Void> newFuture = future.thenAccept(s -> {
+            System.out.println(s + ". Your number's double is " + num * 2 + "!");
+        });
+
+        newFuture.get();
+// this isn't really correct but it's close enough
     }
 }
